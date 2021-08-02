e55e821dcb1811b1adb789d52be0370c95a016bb
Antonio <dontony69@gmail.com>
2021-08-02 19:07:11 +0300

    FirstVersion

---
+++ Client/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string ip = "127.0.0.1";
+            int port = 27654;
+            TcpClient client; 
+            try
+            {
+                client = new TcpClient(ip, port);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("No Connection to the Server");
+                Console.WriteLine("Please try again later!");
+                return;
+            }
+            Console.WriteLine("Connected to the server at: " + client.Client.LocalEndPoint);
+            NetworkStream nwStream = client.GetStream();
+
+            bool programIsRunning = true;
+            while (programIsRunning)
+            {
+                Console.WriteLine("Please insert your currency pair");
+                Console.WriteLine("1. EURUSD 2.USDCAD 3.EURJPY 4.USDGBP 5.USDCHF 6.BTCUSD 0.EXIT!");
+           
+                bool properInputGranted = false;
+                string messageToBeSent = "";
+ 
+                
+                while (!properInputGranted)
+                {
+                    int switcher = 0;
+                    switcher = int.Parse(Console.ReadLine());
+                
+                    switch (switcher)
+                    {
+                        case 0:
+                            messageToBeSent = "Abort";
+                            properInputGranted = true;
+                            programIsRunning = false;
+                            break;
+                        case 1:
+                            messageToBeSent = "EURUSD";
+                            properInputGranted = true;
+                            break;
+                        case 2:
+                            messageToBeSent = "USDCAD";
+                            properInputGranted = true;
+                            break;
+                        case 3:
+                            messageToBeSent = "EURJPY";
+                            properInputGranted = true;
+                            break; 
+                        case 4:
+                            messageToBeSent = "USDGBP";
+                            properInputGranted = true;
+                            break;
+                        case 5:
+                            messageToBeSent = "USDCHF";
+                            properInputGranted = true;
+                            break; 
+                        case 6:
+                            messageToBeSent = "BTCUSD";
+                            properInputGranted = true;
+                            break;
+                        default:
+                            Console.WriteLine("Wrong Choice! Try Again!");
+                            break;
+                    }
+                }
+                Console.WriteLine(messageToBeSent);
+                
+                byte[] bytesToSend = ASCIIEncoding.ASCII.GetBytes(messageToBeSent);
+
+                Console.WriteLine("Sending : " + messageToBeSent);
+                nwStream.Write(bytesToSend, 0, bytesToSend.Length);
+                if (messageToBeSent == "Abort")
+                {
+                    Console.WriteLine("Aborting program!");
+                    return;
+                }
+                
+                byte[] bytesToRead = new byte[client.ReceiveBufferSize];
+                int bytesRead = nwStream.Read(bytesToRead, 0, client.ReceiveBufferSize);
+                Console.WriteLine("Received : " + Encoding.ASCII.GetString(bytesToRead, 0, bytesRead));
+                Console.WriteLine("Exchange rate of " + messageToBeSent + " is " + Encoding.ASCII.GetString(bytesToRead, 0, bytesRead) );
+                
+            }
+           
+            
+            
+            // byte[] bytesToSend = ASCIIEncoding.ASCII.GetBytes(messageToBeSent);
+            //
+            // Console.WriteLine("Sending : " + messageToBeSent);
+            // nwStream.Write(bytesToSend, 0, bytesToSend.Length);
+            //
+            // byte[] bytesToRead = new byte[client.ReceiveBufferSize];
+            // int bytesRead = nwStream.Read(bytesToRead, 0, client.ReceiveBufferSize);
+            // Console.WriteLine("Received : " + Encoding.ASCII.GetString(bytesToRead, 0, bytesRead));
+            // Console.WriteLine("Exchange rate of " + messageToBeSent + " is " + Encoding.ASCII.GetString(bytesToRead, 0, bytesRead) );
+            //
+            
+            Console.ReadLine();
+            client.Close();
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+
+namespace FOReignEXchange
+{
+    internal class Program
+    {
+        
+        public static void Main(string[] args)
+        {
+            
+            string ip = "127.0.0.1";
+            int port = 27654;
+            
+            Dictionary<string, double> CurrencyRates = new Dictionary<string, double>(); 
+            CurrencyRates.Add("EURUSD",1.32);
+            CurrencyRates.Add("USDCAD",1.18);
+            CurrencyRates.Add("EURJPY",123.44);
+            CurrencyRates.Add("USDGBP",0.87);
+            CurrencyRates.Add("USDCHF",0.74);
+            CurrencyRates.Add("BTCUSD",38694.43);
+            
+            TcpListener listener = TcpListener.Create(port);
+            listener.Start();
+            Console.WriteLine("Server started on socket: " + ip + ":" + port);
+            Console.WriteLine("Waiting for the client...");
+
+            TcpClient client = new TcpClient();
+            client = listener.AcceptTcpClient();
+            
+            NetworkStream nwStream = client.GetStream();
+
+            string dataReceived = "";
+
+            
+            while (dataReceived != "Abort")
+            {
+                byte[] buffer = new byte[client.ReceiveBufferSize];
+                int bytesRead = nwStream.Read(buffer, 0, client.ReceiveBufferSize); 
+                
+                
+                dataReceived = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                Console.WriteLine("Received : " + dataReceived);
+
+                double dataToBeSent = 0;
+
+                switch (dataReceived)
+                {
+                    case "EURUSD":
+                        dataToBeSent = CurrencyRates["EURUSD"];
+                        break;
+                    case "USDCAD":
+                        dataToBeSent = CurrencyRates["USDCAD"];
+                        break;
+                    case "EURJPY":
+                        dataToBeSent = CurrencyRates["EURJPY"];
+                        break;
+                    case "USDGBP":
+                        dataToBeSent = CurrencyRates["USDGBP"];
+                        break;
+                    case "USDCHF":
+                        dataToBeSent = CurrencyRates["USDCHF"];
+                        break;
+                    case "BTCUSD":
+                        dataToBeSent = CurrencyRates["BTCUSD"];
+                        break;
+                    case "Abort":
+                        client.Close();
+                        listener.Stop();
+                        Console.WriteLine("GoodBye!");
+                        return;
+                    default:
+                        Console.WriteLine("Wrong message form the Client");
+                        break;
+                }
+                Console.WriteLine("Sending back : " + dataToBeSent);
+                byte[] buffer1 = Encoding.ASCII.GetBytes(dataToBeSent.ToString());
+                nwStream.Write(buffer1, 0, buffer1.Length);
+                
+            }
+            
+          
+            //
+            // dataReceived = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+            // Console.WriteLine("Received : " + dataReceived);
+            //
+            // double dataToBeSent = 0;
+            //
+            // switch (dataReceived)
+            // {
+            //     case "EURUSD":
+            //         dataToBeSent = CurrencyRates["EURUSD"];
+            //         break;
+            //     case "USDCAD":
+            //         dataToBeSent = CurrencyRates["USDCAD"];
+            //         break;
+            //     case "EURJPY":
+            //         dataToBeSent = CurrencyRates["EURJPY"];
+            //         break;
+            //     case "USDGBP":
+            //         dataToBeSent = CurrencyRates["USDGBP"];
+            //         break;
+            //     case "USDCHF":
+            //         dataToBeSent = CurrencyRates["USDCHF"];
+            //         break;
+            //     case "BTCUSD":
+            //         dataToBeSent = CurrencyRates["BTCUSD"];
+            //         break;
+            //     default:
+            //         Console.WriteLine("Wrong message form the Client");
+            //         break;
+            // }
+            // Console.WriteLine("Sending back : " + dataToBeSent);
+            // byte[] buffer1 = Encoding.ASCII.GetBytes(dataToBeSent.ToString());
+            // nwStream.Write(buffer1, 0, buffer1.Length);
+            
+            client.Close();
+            listener.Stop();
+            Console.ReadLine();
+            
+        }
+    }
+}
